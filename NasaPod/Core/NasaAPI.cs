b60a46b9b0235c4ca187d185a9f4abe621f8c635
Nerdomante88 <ilcorax@gmail.com>
2023-06-05 01:43:13 +0200

    manage responses not 200

--- NasaPod/Core/NasaAPI.cs
+++ NasaPod/Core/NasaAPI.cs
@@ -19,24 +19,95 @@ namespace Nasa.Core
             request.AddQueryParameter("api_key", settings.ApiKey);
 
             RestResponse response = await client.ExecuteAsync(request);
-
-            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            switch (response.StatusCode)
             {
-                if (response.Content != null)
-                {
-                    APOD respObject = JsonSerializer.Deserialize<APOD>(response.Content);
-                    return respObject != null ? respObject : new APOD();
-                }
-                else
-                {
-                    throw new Exception("Content null");
-                }
-            }
-            else
-            {
-                throw new Exception(response.ErrorMessage);
+                case System.Net.HttpStatusCode.OK:
+                    if (response.Content != null)
+                    {
+                        APOD respObject = JsonSerializer.Deserialize<APOD>(response.Content);
+                        return respObject != null ? respObject : new APOD();
+                    }
+                    else
+                    {
+                        throw new Exception("Content null");
+                    }
+                case System.Net.HttpStatusCode.BadRequest:
+                    throw new Exception("Bad request");
+                case System.Net.HttpStatusCode.Unauthorized:
+                    throw new Exception("Unauthorized");
+                case System.Net.HttpStatusCode.Forbidden:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.NotFound:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.MethodNotAllowed:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.NotAcceptable:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.ProxyAuthenticationRequired:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.RequestTimeout:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.Conflict:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.Gone:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.LengthRequired:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.PreconditionFailed:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.RequestEntityTooLarge:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.RequestUriTooLong:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.UnsupportedMediaType:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.RequestedRangeNotSatisfiable:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.ExpectationFailed:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.MisdirectedRequest:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.UnprocessableEntity:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.Locked:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.FailedDependency:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.UpgradeRequired:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.PreconditionRequired:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.TooManyRequests:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.RequestHeaderFieldsTooLarge:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.UnavailableForLegalReasons:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.InternalServerError:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.NotImplemented:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.BadGateway:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.ServiceUnavailable:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.GatewayTimeout:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.HttpVersionNotSupported:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.VariantAlsoNegotiates:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.InsufficientStorage:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.LoopDetected:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.NotExtended:
+                    throw new Exception("Temporary unavaiable");
+                case System.Net.HttpStatusCode.NetworkAuthenticationRequired:
+                    throw new Exception("Temporary unavaiable");
+                default:
+                    throw new Exception(response.StatusCode.ToString());
             }
         }
-
     }
 }
