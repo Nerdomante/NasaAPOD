9f602175ebbaa482369890fbd09255d3cde37d78
Nerdomante88 <ilcorax@gmail.com>
2023-06-05 01:45:10 +0200

    manage response not 200

--- NasaPod/Core/NasaAPI.cs
+++ NasaPod/Core/NasaAPI.cs
@@ -36,75 +36,75 @@ namespace Nasa.Core
                 case System.Net.HttpStatusCode.Unauthorized:
                     throw new Exception("Unauthorized");
                 case System.Net.HttpStatusCode.Forbidden:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.NotFound:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.MethodNotAllowed:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.NotAcceptable:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.ProxyAuthenticationRequired:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.RequestTimeout:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.Conflict:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.Gone:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.LengthRequired:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.PreconditionFailed:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.RequestEntityTooLarge:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.RequestUriTooLong:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.UnsupportedMediaType:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.RequestedRangeNotSatisfiable:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.ExpectationFailed:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.MisdirectedRequest:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.UnprocessableEntity:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.Locked:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.FailedDependency:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.UpgradeRequired:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.PreconditionRequired:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.TooManyRequests:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.RequestHeaderFieldsTooLarge:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.UnavailableForLegalReasons:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.InternalServerError:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.NotImplemented:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.BadGateway:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.ServiceUnavailable:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.GatewayTimeout:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.HttpVersionNotSupported:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.VariantAlsoNegotiates:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.InsufficientStorage:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.LoopDetected:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.NotExtended:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 case System.Net.HttpStatusCode.NetworkAuthenticationRequired:
-                    throw new Exception("Temporary unavaiable");
+                    throw new Exception("Service temporary unavaiable");
                 default:
                     throw new Exception(response.StatusCode.ToString());
             }
